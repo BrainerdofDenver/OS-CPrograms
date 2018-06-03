008347f1a6a283ed5698f71ed5fc9d8e770d43b9
Andrew S Brainerd <abraine1@msudenver.edu>
2018-06-03 15:48:08 -0600

    Create counter.cc

---
+++ counter.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[]){
+	int parameter = (long)argv[1];
+	int i;
+	pid_t currentPID = getpid();
+	
+	for(i = 1; i < 6; i++){
+		printf("\nProcess: %d", currentPID);
+		printf(" %d", i, "\n\n");
+	}
+	return parameter;
+}
